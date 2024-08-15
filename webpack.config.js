f552508de479855064463e2114d22ef900cb081e
Joy Debnath <96875662+developerjoydebnath@users.noreply.github.com>
2024-08-16 00:18:49 +0600

    updated

--- webpack.config.js
+++ webpack.config.js
@@ -1,56 +1,64 @@
+// webpack.config.js
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  mode: "development",
   entry: "./src/app.js",
   output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
-    clean: true, // Automatically clean the output directory before each build
-  },
-  devServer: {
-    static: {
-      directory: path.join(__dirname, "dist"),
-    },
-    open: true,
-    port: 3000,
-    hot: true, // Enable hot module replacement
-    liveReload: true, // Enable live reloading
+    filename: "js/bundle.js",
+    path: path.resolve(__dirname, "public/assets/"),
   },
   module: {
     rules: [
+      // JavaScript
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env"],
-          },
-        },
+        use: "babel-loader",
       },
+      // SCSS
       {
         test: /\.scss$/,
         use: [
           MiniCssExtractPlugin.loader,
-          "css-loader", // Interprets @import and url() like import/require() and will resolve them
-          "postcss-loader", // Process CSS with PostCSS
-          "sass-loader", // Loads a Sass/SCSS file and compiles it to CSS
+          "css-loader",
+          "postcss-loader",
+          "sass-loader",
         ],
+      },
+      // Images
+      {
+        test: /\.(png|jpe?g|gif|svg)$/,
+        type: "asset/resource",
+        generator: {
+          filename: "images/[name][ext][query]",
+        },
+      },
+      // Fonts
+      {
+        test: /\.(woff(2)?|eot|ttf|otf)$/,
+        type: "asset/resource",
+        generator: {
+          filename: "fonts/[name][ext][query]",
+        },
       },
     ],
   },
   plugins: [
-    new CleanWebpackPlugin(), // Cleans the output directory before each build
-    new HtmlWebpackPlugin({
-      template: "./src/index.html",
-      filename: "index.html",
-    }),
     new MiniCssExtractPlugin({
-      filename: "styles.css", // Output CSS file name
+      filename: "css/styles.css",
     }),
   ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "public"),
+    },
+    compress: true,
+    port: 9000,
+    open: true,
+    historyApiFallback: true,
+  },
+  resolve: {
+    extensions: [".js", ".scss"],
+  },
 };
